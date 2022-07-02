609d312b69851c48b44b8ee8b82635e45dcb62c0
jeremycos <neocirk@gmail.com>
2022-07-03 00:27:21 +0300

    test command line

--- index.js
+++ index.js
@@ -1,78 +1,80 @@
-var path = require('path');
-const fs = require('fs');
-const fetch = (...args) =>
-  import('node-fetch').then(({default: fetch}) => fetch(...args));
-var Validator = require('jsonschema').Validator;
-var v = new Validator();
-const {executeCommand} = require('./executecommand');
+#! /usr/bin/env node
 
-const directory='../..';
+var path = require("path");
+const fs = require("fs");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+var Validator = require("jsonschema").Validator;
+var v = new Validator();
+const { executeCommand } = require("./executecommand");
+
+const directory = "../..";
 
 //Schemas
 var variableSchema = {
-  id: '/Variable',
-  type: 'object',
+  id: "/Variable",
+  type: "object",
   properties: {
-    name: {type: 'string'},
-    defaultValue: {type: 'string'},
+    name: { type: "string" },
+    defaultValue: { type: "string" },
   },
-  required: ['name', 'defaultValue'],
+  required: ["name", "defaultValue"],
 };
 
 var lineSchema = {
-  id: '/Line',
-  type: 'object',
+  id: "/Line",
+  type: "object",
   properties: {
-    reference: {type: 'string', minLength: 6},
-    code: {type: 'string'},
-    isUnique: {type: 'bool'},
-    type: {type: 'string'},
-    variables: {type: 'array', items: {$ref: '/Variable'}},
+    reference: { type: "string", minLength: 6 },
+    code: { type: "string" },
+    isUnique: { type: "bool" },
+    type: { type: "string" },
+    variables: { type: "array", items: { $ref: "/Variable" } },
   },
-  required: ['reference', 'code', 'isUnique', 'type'],
+  required: ["reference", "code", "isUnique", "type"],
 };
 
 var instructionSchema = {
-  id: '/Instruction',
-  type: 'object',
+  id: "/Instruction",
+  type: "object",
   properties: {
-    file: {type: 'string'},
-    mode: {type: 'string'},
-    actions: {type: 'array', items: {$ref: '/Line'}},
+    file: { type: "string" },
+    mode: { type: "string" },
+    actions: { type: "array", items: { $ref: "/Line" } },
   },
-  required: ['file', 'mode', 'actions'],
+  required: ["file", "mode", "actions"],
 };
 
 var schema = {
-  id: '/Setup',
-  type: 'object',
+  id: "/Setup",
+  type: "object",
   properties: {
-    uid: {type: 'string'},
-    name: {type: 'string'},
-    description: {type: 'string'},
-    instructions: {type: 'array', items: {$ref: '/Instruction'}},
+    uid: { type: "string" },
+    name: { type: "string" },
+    description: { type: "string" },
+    instructions: { type: "array", items: { $ref: "/Instruction" } },
   },
-  required: ['uid', 'name', 'description', 'instructions'],
+  required: ["uid", "name", "description", "instructions"],
 };
 
-v.addSchema(instructionSchema, '/Instruction');
-v.addSchema(lineSchema, '/Line');
-v.addSchema(variableSchema, '/Variable');
+v.addSchema(instructionSchema, "/Instruction");
+v.addSchema(lineSchema, "/Line");
+v.addSchema(variableSchema, "/Variable");
 
 let urlTest =
-  'https://storage.googleapis.com/rocketsetup-2ded0.appspot.com/insertRocket.json';
-let settings = {method: 'Get'};
+  "https://storage.googleapis.com/rocketsetup-2ded0.appspot.com/insertRocket.json";
+let settings = { method: "Get" };
 
 async function getSetupJson(url, settings) {
-  return await fetch(url, settings).then(res => {
+  return await fetch(url, settings).then((res) => {
     return res.json();
   });
 }
 
 const messages = {
-  noFileFound: '❌ No file found in the directory',
+  noFileFound: "❌ No file found in the directory",
   manySameFiles:
-    '❌ There are many files with the same name. Here is the list : ',
+    "❌ There are many files with the same name. Here is the list : ",
 };
 
 /*
@@ -87,136 +89,136 @@ function findFile(instruction, params) {
   const fileName = instruction?.file;
 
   return new Promise((resolve, reject) => {
-    var finder = require('findit')(directory);
+    var finder = require("findit")(directory);
     const files = [];
 
     var totalFilesFound = 0;
 
-    finder.on('file', function (file) {
+    finder.on("file", function (file) {
       if (file.includes(fileName)) {
         totalFilesFound++;
         files.push(file);
       }
     });
 
-    finder.on('error', reject);
+    finder.on("error", reject);
 
-    finder.on('end', () => {
-      console.log('\x1b[0m', `\nStarting instruction...`);
+    finder.on("end", () => {
+      console.log("\x1b[0m", `\nStarting instruction...`);
 
       if (totalFilesFound === 0) {
-        console.log('\x1b[31m', messages.noFileFound);
+        console.log("\x1b[31m", messages.noFileFound);
       }
 
       if (totalFilesFound === 1) {
         const file = files[0];
 
-        var newData = '';
+        var newData = "";
 
-        console.log('\x1b[32m', '✅ File found: ', file);
+        console.log("\x1b[32m", "✅ File found: ", file);
 
         //if the file is found, we read the content of it and print it to the console
-        fs.readFile(file, 'utf8', (err, data) => {
+        fs.readFile(file, "utf8", (err, data) => {
           if (err) {
             console.error(err);
             return;
           }
 
-          instruction?.actions?.forEach(item => {
+          instruction?.actions?.forEach((item) => {
             var isExist = data.indexOf(item.reference) >= 0;
 
             //Check & get variables
             if (item?.variables?.length > 0) {
-              item.variables.map(variable => {
+              item.variables.map((variable) => {
                 var check = variable?.name in params;
 
                 if (check) {
                   item.code = item.code.replace(
                     `%${variable.name}`,
-                    params[variable.name],
+                    params[variable.name]
                   );
 
                   console.log(
-                    '\x1b[32m',
-                    '✅ Replaced  %' +
+                    "\x1b[32m",
+                    "✅ Replaced  %" +
                       variable?.name +
-                      ' with ' +
-                      params[variable.name],
+                      " with " +
+                      params[variable.name]
                   );
                 }
               });
             }
 
-            if (item.type === 'add_before') {
+            if (item.type === "add_before") {
               if (isExist) {
                 console.log(
-                  '\x1b[32m',
+                  "\x1b[32m",
                   '✅ Insert before code: Expression "' +
                     item.reference +
-                    '" found',
+                    '" found'
                 );
                 if (item.isUnique) {
                   newData = data.replace(
                     item.reference,
-                    item.code + item.reference,
+                    item.code + item.reference
                   ); //Insert before
                 } else {
                   newData = data.replaceAll(
                     item.reference,
-                    item.code + item.reference,
+                    item.code + item.reference
                   );
                 }
               } else {
                 console.log(
-                  '\x1b[31m',
+                  "\x1b[31m",
                   '❌ Insert before code: Expression "' +
                     item.reference +
-                    '" not found',
+                    '" not found'
                 );
               }
             }
-            if (item.type === 'add_after') {
+            if (item.type === "add_after") {
               if (isExist) {
                 console.log(
-                  '\x1b[32m',
+                  "\x1b[32m",
                   '✅ Insert after code: Expression "' +
                     item.reference +
-                    '" found',
+                    '" found'
                 );
                 if (item.isUnique) {
                   newData = newData.replace(
                     item.reference,
-                    item.reference + item.code,
+                    item.reference + item.code
                   ); //Insert before
                 } else {
                   newData = newData.replaceAll(
                     item.reference,
-                    item.reference + item.code,
+                    item.reference + item.code
                   );
                 }
               } else {
                 console.log(
-                  '\x1b[31m',
+                  "\x1b[31m",
                   '❌ Insert after code: Expression "' +
                     item.reference +
-                    '" not found',
+                    '" not found'
                 );
               }
             }
           });
 
-          if (newData !== data && newData !== '') {
-            fs.writeFile(file, newData, {flag: 'w+'}, () =>
-              console.log('\x1b[32m', '✅ File modified: ', file),
+          if (newData !== data && newData !== "") {
+            fs.writeFile(file, newData, { flag: "w+" }, () =>
+              console.log("\x1b[32m", "✅ File modified: ", file)
             );
           } else {
-            console.log('\x1b[31m', '❌ File not modified');
+            console.log("\x1b[31m", "❌ File not modified");
           }
         });
       }
 
       if (totalFilesFound > 1) {
-        console.log('\x1b[31m', messages.manySameFiles, files);
+        console.log("\x1b[31m", messages.manySameFiles, files);
       }
 
       return resolve(Promise.all(files));
@@ -228,38 +230,40 @@ function findFile(instruction, params) {
 }
 
 (async function main() {
-  var argv = require('minimist')(process.argv.slice(2));
+  var argv = require("minimist")(process.argv.slice(2));
 
-  if (argv?.url?.length > 0 && argv?.url?.includes('.json')) {
+  if (argv?.url?.length > 0 && argv?.url?.includes(".json")) {
     const json = await getSetupJson(argv.url, settings);
 
-    const isValidFormat = v.validate(json, schema, {nestedErrors: true}).valid;
+    const isValidFormat = v.validate(json, schema, {
+      nestedErrors: true,
+    }).valid;
 
     var params = {};
 
     console.log(process.argv);
     if (argv?.params?.length > 0) {
       argv?.params
-        .split('+')
-        .forEach(part => (params[part.split('=')[0]] = part.split('=')[1]));
+        .split("+")
+        .forEach((part) => (params[part.split("=")[0]] = part.split("=")[1]));
     }
 
     if (isValidFormat) {
-      console.log('\x1b[32m', '\nJSON provided is correct');
+      console.log("\x1b[32m", "\nJSON provided is correct");
 
       for (instruction of json.instructions) {
         await findFile(instruction, params);
       }
     } else {
       console.log(
-        '\x1b[31m',
-        '\nNo valid json format. Please read the doc to provide a correct json file.',
+        "\x1b[31m",
+        "\nNo valid json format. Please read the doc to provide a correct json file."
       );
     }
   } else {
     console.log(
-      '\x1b[31m',
-      '\nNo valid url. Use --url yoururl.json as arguments',
+      "\x1b[31m",
+      "\nNo valid url. Use --url yoururl.json as arguments"
     );
   }
 })();
